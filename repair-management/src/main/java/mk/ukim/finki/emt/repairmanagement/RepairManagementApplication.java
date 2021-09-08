f83f1ff042093f59a3457ea91f774f0064bc5a5b
Dimitar Papalazov <dimitar-papalazov@students.finki.ukim.mk>
2021-09-08 09:40:46 +0200

    Add product-management & repair-management

---
+++ repair-management/src/main/java/mk/ukim/finki/emt/repairmanagement/RepairManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.repairmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairManagementApplication.class, args);
+    }
+
+}
---
+++ repair-management/src/test/java/mk/ukim/finki/emt/repairmanagement/RepairManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.repairmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
